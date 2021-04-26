541fe5949ca6cac90e461df8c8ab6ba285223e7a
小菜 蔡 <grace22875001@livemail.tw>
2021-04-26 17:01:37 +0800

    first edition

---
+++ auth/Data/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using auth.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auth.Data
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email).IsUnique();
+            });
+        }
+    }
+}
---
+++ auth/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace auth.Dtos
+{
+    public class LoginDto
+    {
+        public string Email { set; get; }
+        public string Password { set; get; }
+    }
+}
